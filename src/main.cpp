a07128b6e599bacebc3ea48aa33a5096d7f4e005
Stanisław Zagórowski <hasohasacz@gmail.com>
2024-09-14 12:46:33 +0200

    working C API version

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,24 @@
 // Required for use of glslang_default_resource
 #include <glslang/Public/resource_limits_c.h>
 
-typedef struct SpirVBinary {
-    uint32_t *words; // SPIR-V words
-    int size; // number of words in SPIR-V binary
-} SpirVBinary;
+#include <optional>
+#include <string_view>
+#include <string>
+#include <fstream>
 
-SpirVBinary compileShaderToSPIRV_Vulkan(glslang_stage_t stage, const char* shaderSource, const char* fileName) {
+#include <stdio.h>
+#include <stdint.h>
+
+struct SpirVBinary {
+    uint32_t* words; // SPIR-V words
+    int size;        // number of words in SPIR-V binary
+};
+
+SpirVBinary compileShaderToSPIRV_Vulkan(
+    glslang_stage_t stage,
+    const char* shaderSource,
+    const char* fileName
+) {
     const glslang_input_t input = {
         .language = GLSLANG_SOURCE_GLSL,
         .stage = stage,
@@ -23,6 +35,8 @@ SpirVBinary compileShaderToSPIRV_Vulkan(glslang_stage_t stage, const char* shade
         .forward_compatible = false,
         .messages = GLSLANG_MSG_DEFAULT_BIT,
         .resource = glslang_default_resource(),
+        .callbacks = NULL,
+        .callbacks_ctx = NULL,
     };
 
     glslang_shader_t* shader = glslang_shader_create(&input);
@@ -31,7 +45,7 @@ SpirVBinary compileShaderToSPIRV_Vulkan(glslang_stage_t stage, const char* shade
         .words = NULL,
         .size = 0,
     };
-    if (!glslang_shader_preprocess(shader, &input))	{
+    if (!glslang_shader_preprocess(shader, &input)) {
         printf("GLSL preprocessing failed %s\n", fileName);
         printf("%s\n", glslang_shader_get_info_log(shader));
         printf("%s\n", glslang_shader_get_info_debug_log(shader));
@@ -52,7 +66,10 @@ SpirVBinary compileShaderToSPIRV_Vulkan(glslang_stage_t stage, const char* shade
     glslang_program_t* program = glslang_program_create();
     glslang_program_add_shader(program, shader);
 
-    if (!glslang_program_link(program, GLSLANG_MSG_SPV_RULES_BIT | GLSLANG_MSG_VULKAN_RULES_BIT)) {
+    if (!glslang_program_link(
+            program,
+            GLSLANG_MSG_SPV_RULES_BIT | GLSLANG_MSG_VULKAN_RULES_BIT
+        )) {
         printf("GLSL linking failed %s\n", fileName);
         printf("%s\n", glslang_program_get_info_log(program));
         printf("%s\n", glslang_program_get_info_debug_log(program));
@@ -64,7 +81,7 @@ SpirVBinary compileShaderToSPIRV_Vulkan(glslang_stage_t stage, const char* shade
     glslang_program_SPIRV_generate(program, stage);
 
     bin.size = glslang_program_SPIRV_get_size(program);
-    bin.words = malloc(bin.size * sizeof(uint32_t));
+    bin.words = (uint32_t*)malloc(bin.size * sizeof(uint32_t));
     glslang_program_SPIRV_get(program, bin.words);
 
     const char* spirv_messages = glslang_program_SPIRV_get_messages(program);
@@ -75,4 +92,117 @@ SpirVBinary compileShaderToSPIRV_Vulkan(glslang_stage_t stage, const char* shade
     glslang_shader_delete(shader);
 
     return bin;
+}
+
+struct Args {
+    std::string inputFile;
+    std::string outputFile;
+    glslang_stage_t stage;
+
+    static glslang_stage_t guessStageFromFileName(const std::string& fileName) {
+        if (fileName.find(".vert") != std::string::npos) {
+            return GLSLANG_STAGE_VERTEX;
+        } else if (fileName.find(".frag") != std::string::npos) {
+            return GLSLANG_STAGE_FRAGMENT;
+        } else if (fileName.find(".comp") != std::string::npos) {
+            return GLSLANG_STAGE_COMPUTE;
+        } else {
+            return GLSLANG_STAGE_VERTEX;
+        }
+    }
+
+    Args(int argc, char* argv[]) {
+        std::optional<std::string> inputFile;
+        std::optional<std::string> outputFile;
+        std::optional<glslang_stage_t> stage;
+
+        for (int i = 1; i < argc; i++) {
+            std::string_view arg = argv[i];
+
+            if (arg == "-o") {
+                if (i + 1 < argc) {
+                    outputFile = argv[++i];
+                } else {
+                    printf("No output file specified\n");
+                    exit(1);
+                }
+            } else if (arg == "-s") {
+                if (i + 1 < argc) {
+                    std::string_view stageStr = argv[++i];
+                    if (stageStr == "vert" || stageStr == "vertex") {
+                        stage = GLSLANG_STAGE_VERTEX;
+                    } else if (stageStr == "frag" || stageStr == "fragment") {
+                        stage = GLSLANG_STAGE_FRAGMENT;
+                    } else if (stageStr == "comp" || stageStr == "compute") {
+                        stage = GLSLANG_STAGE_COMPUTE;
+                    } else {
+                        printf("Unknown stage %s\n", stageStr.data());
+                        exit(1);
+                    }
+                } else {
+                    printf("No stage specified\n");
+                    exit(1);
+                }
+            } else {
+                inputFile = arg;
+            }
+        }
+
+        if (inputFile) {
+            this->inputFile = *inputFile;
+            if (!outputFile) {
+                this->outputFile = this->inputFile + ".spv";
+            } else {
+                this->outputFile = *outputFile;
+            }
+            if (stage) {
+                this->stage = *stage;
+            } else {
+                this->stage = guessStageFromFileName(this->inputFile);
+            }
+        } else {
+            printf("No input file specified\n");
+            exit(1);
+        }
+    }
+};
+
+int main(int argc, char* argv[]) {
+    Args args(argc, argv);
+
+    glslang_initialize_process();
+
+    std::ifstream file(args.inputFile);
+    if (!file.is_open()) {
+        printf("Failed to open file %s\n", args.inputFile.c_str());
+        return 1;
+    }
+
+    std::string shaderSource(
+        (std::istreambuf_iterator<char>(file)),
+        std::istreambuf_iterator<char>()
+    );
+
+    SpirVBinary bin = compileShaderToSPIRV_Vulkan(
+        args.stage,
+        shaderSource.c_str(),
+        args.inputFile.c_str()
+    );
+
+    if (bin.words) {
+        std::ofstream outFile(args.outputFile, std::ios::binary);
+        if (!outFile.is_open()) {
+            printf("Failed to open file %s\n", args.outputFile.c_str());
+            return 1;
+        }
+
+        outFile.write((char*)bin.words, bin.size * sizeof(uint32_t));
+        outFile.close();
+
+        free(bin.words);
+    }
+
+    glslang_finalize_process();
+
+    return 0;
 }
